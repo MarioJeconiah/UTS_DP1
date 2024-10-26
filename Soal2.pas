c137d1af3e127c7cd4f46dab22cd7c13ae3b2d7e
MarioJeconiah <mariopurba40@gmail.com>
2024-10-26 15:50:44 +0700

    Update Soal2.pas

--- Soal2.pas
+++ Soal2.pas
@@ -1,1 +1,25 @@
-//ketik kode jawaban no 2 kamu disini
+uses crt; // untuk import unit crt yang kita pake di built in function clrscr
+
+
+var //mendeklarasikan variabel
+    jumlah_pelanggan, jumlah_jenis_sayur, jumlah_sayur, i, y: integer; //variabel jumlah pelanggan, jumlah jenis dan sayur tipe integer
+    total, harga_sayur: real; //total dan harga sayur bertipe data real
+begin // mulai program
+    clrscr; //built in function untuk membersihkan terminal
+    write('Masukkan jumlah pelanggan :'); readln(jumlah_pelanggan); // tampilan 'Masukkan jumlah pelanggan :' dan input variabel jumlah_pelanggan
+
+    for i := 1 to jumlah_pelanggan do // pengulangan dari 1 sampai jumlah_pelanggan
+    begin // mulai untuk pengulangan sampai ke jumlah pelanggan
+        total := 0; // inisialisasi nilai total jadi 0 agar data tidak bertumpuk pada pengulangan berikutnya
+        writeln('Pelanggan ke-',i); // tampilan 'Pelanggan ke-' i yakni pengulangan ke 1 
+        write('Masukkan jumlah jenis sayur yang dibeli :'); readln(jumlah_jenis_sayur); // tampilan 'Masukkan jumlah jenis sayur yang dibeli :' dan input jumlah_jenis_sayur
+
+        for y := 1 to jumlah_jenis_sayur do // pengulangan untuk jumlah jenis sayur 
+        begin // mulai untuk pengulangan jumlah jenis sayur
+            write('Masukkan harga sayur ke-', y ,' :'); readln(harga_sayur); // tampilan 'Masukkan harga sayur ke-' y sesuai pengulangan dan input harga sayur
+            write('Masukkan jumlah sayur yang dibeli :'); readln(jumlah_sayur); // tampilan 'Masukkan jumlah sayur yang dibeli :'' dan input jumlah sayur
+            total := (harga_sayur * jumlah_sayur) + total; // inisialisasi total adalah harga sayur dikali jumlah sayur dan ditambah nilai total pada pengulangan sebelumnya
+        end; // pengulangan jumlah jenis sayur selesai
+        writeln('Total belanja untuk pelanggan ke-', i ,': Rp', total :3 :2); // tampilan 'Total belanja unutk pelanggan ke-' i yakni pengulangan ': Rp' lalu tampilan nilai total 
+    end; // pengulangan pelanggan selesai
+end. // program utama selesai
